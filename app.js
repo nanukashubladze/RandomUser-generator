79939d80d97e3fc5ffc27c180a26df4b60a633ed
nanukashubladze <123475291+nanukashubladze@users.noreply.github.com>
2023-12-08 20:02:03 +0400

    tittle change

--- app.js
+++ app.js
@@ -5,15 +5,17 @@ const app = Vue.createApp({
       lastName: 'Doe',
       email: 'john@gmail.com',
       gender: 'male',
-      picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+      picture: 'https://randomuser.me/api/portraits/men/1.jpg',
     }
   },
   methods: {
     async getUser() {
       const res = await fetch('https://randomuser.me/api')
       const { results } = await res.json()
+      console.log(results)
+      
 
-      // console.log(results)
+      
 
       this.firstName = results[0].name.first
       this.lastName = results[0].name.last
